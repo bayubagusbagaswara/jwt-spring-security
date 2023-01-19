ea7f90a6a2af55d764dee1baced1d35677f87bd5
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-19 09:48:19 +0700

    add model User

---
+++ src/main/java/com/bayu/jwt/model/User.java
@@ -0,0 +1,106 @@
+package com.bayu.jwt.model;
+
+import lombok.*;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"email"}, name = "user_email_unique"),
+        @UniqueConstraint(columnNames ={"username"}, name = "user_username_unique")
+})
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", allocationSize = 1)
+    private Long id;
+
+    @NaturalId
+    @Column(name = "email")
+    @NotBlank(message = "User email cannot be null")
+    private String email;
+
+    @Column(name = "username")
+    @NotBlank(message = "Username cannot be blank")
+    private String username;
+
+    @Column(name = "password")
+    @NotNull(message = "Password cannot be null")
+    private String password;
+
+    @Column(name = "first_name")
+    @NotBlank(message = "First name can not be blank")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotBlank(message = "Last name can not be blank")
+    private String lastName;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean active;
+
+    // TABLE MANY TO MANY BETWEEN USER AND ROLE
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "role_id")}
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    @Column(name = "is_email_verified", nullable = false)
+    private Boolean isEmailVerified;
+
+
+    public void addRole(Role role) {
+        roles.add(role);
+        role.getUserList().add(this);
+    }
+
+    public void addRoles(Set<Role> roles) {
+        roles.forEach(this::addRole);
+    }
+
+    public void removeRole(Role role) {
+        roles.remove(role);
+        role.getUserList().remove(this);
+    }
+
+    public void markVerificationConfirmed() {
+        setEmailVerified(true);
+    }
+
+    public Boolean getEmailVerified() {
+        return isEmailVerified;
+    }
+
+    public void setEmailVerified(Boolean emailVerified) {
+        isEmailVerified = emailVerified;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", active=" + active +
+                ", roles=" + roles +
+                ", isEmailVerified=" + isEmailVerified +
+                '}';
+    }
+}
