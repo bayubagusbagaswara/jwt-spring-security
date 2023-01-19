b30b35acc084c3043ed8ab1d2449600841031ac8
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-19 17:21:48 +0700

    add contract service PasswordResetTokenService

---
+++ src/main/java/com/bayu/jwt/service/PasswordResetTokenService.java
@@ -0,0 +1,21 @@
+package com.bayu.jwt.service;
+
+import com.bayu.jwt.model.PasswordResetToken;
+import com.bayu.jwt.model.User;
+import com.bayu.jwt.payload.PasswordResetRequest;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenService {
+
+    public PasswordResetToken getValidToken(PasswordResetRequest request);
+
+    public Optional<PasswordResetToken> createToken(User user);
+
+    public PasswordResetToken claimToken(PasswordResetToken token);
+
+    public void verifyExpiration(PasswordResetToken token);
+
+    public void matchEmail(PasswordResetToken token, String requestEmail);
+
+}
