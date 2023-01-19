c250fdc2aaef69efc7778b950c70e877c39e9bed
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-19 14:42:24 +0700

    add payload ApiResponse

---
+++ src/main/java/com/bayu/jwt/payload/ApiResponse.java
@@ -0,0 +1,31 @@
+package com.bayu.jwt.payload;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.Instant;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse {
+
+    private final String data;
+    private final Boolean success;
+    private final String timestamp;
+    private final String cause;
+    private final String path;
+
+    public ApiResponse(Boolean success, String data, String cause, String path) {
+        this.timestamp = Instant.now().toString();
+        this.data = data;
+        this.success = success;
+        this.cause = cause;
+        this.path = path;
+    }
+
+    public ApiResponse(Boolean success, String data) {
+        this.timestamp = Instant.now().toString();
+        this.data = data;
+        this.success = success;
+        this.cause = null;
+        this.path = null;
+    }
+}
