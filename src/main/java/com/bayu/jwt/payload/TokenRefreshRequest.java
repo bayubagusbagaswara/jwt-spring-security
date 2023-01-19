533f197f01b2927898bbb48bd3b6211d1d47b00c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-19 16:13:54 +0700

    add payload TokenRefreshRequest

---
+++ src/main/java/com/bayu/jwt/payload/TokenRefreshRequest.java
@@ -0,0 +1,17 @@
+package com.bayu.jwt.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRefreshRequest {
+
+    @NotBlank(message = "Refresh token cannot be blank")
+    private String refreshToken;
+
+}
