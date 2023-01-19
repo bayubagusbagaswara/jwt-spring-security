b9565692772c6c5d741435db660f57f011138e30
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-19 14:48:42 +0700

    add payload JwtAuthenticationResponse

---
+++ src/main/java/com/bayu/jwt/payload/JwtAuthenticationResponse.java
@@ -0,0 +1,26 @@
+package com.bayu.jwt.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private Long expiryDuration;
+
+    public JwtAuthenticationResponse(String accessToken, String refreshToken, Long expiryDuration) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.expiryDuration = expiryDuration;
+        tokenType = "Bearer ";
+    }
+
+}
