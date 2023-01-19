c78a8f480a1da6fa7a0604789512528216ab0355
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-19 14:51:20 +0700

    add model Mail

---
+++ src/main/java/com/bayu/jwt/model/Mail.java
@@ -0,0 +1,26 @@
+package com.bayu.jwt.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mail {
+
+    private String from;
+    private String to;
+    private String subject;
+    private String content;
+    private Map<String, String> model;
+
+    public Mail() {
+        model = new HashMap<>();
+    }
+
+    public Mail(String from, String to, String subject, String content, Map<String, String> model) {
+        this.from = from;
+        this.to = to;
+        this.subject = subject;
+        this.content = content;
+        this.model = model;
+    }
+
+}
