459abaed2d52d32928b2d514ec57c8756b09ab93
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-20 10:05:19 +0700

    add method contract updateExistingTokenWithNameAndExpiry, findByToken, save, generateNewToken, verifyExpiration

--- src/main/java/com/bayu/jwt/service/EmailVerificationTokenService.java
+++ src/main/java/com/bayu/jwt/service/EmailVerificationTokenService.java
@@ -1,4 +1,18 @@
 package com.bayu.jwt.service;
 
+import com.bayu.jwt.model.token.EmailVerificationToken;
+
+import java.util.Optional;
+
 public interface EmailVerificationTokenService {
+
+    public EmailVerificationToken updateExistingTokenWithNameAndExpiry(EmailVerificationToken existingToken);
+
+    public Optional<EmailVerificationToken> findByToken(String token);
+
+    public EmailVerificationToken save(EmailVerificationToken emailVerificationToken);
+
+    public String generateNewToken();
+
+    public void verifyExpiration(EmailVerificationToken token);
 }
