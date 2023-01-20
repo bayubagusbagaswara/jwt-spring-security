2a0bf8ff5dede2e71795aef8fa1c1555dd7c5a08
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-20 09:50:18 +0700

    add method loadUserByUsername and loadUserById

--- src/main/java/com/bayu/jwt/service/CustomUserDetailsService.java
+++ src/main/java/com/bayu/jwt/service/CustomUserDetailsService.java
@@ -1,6 +1,13 @@
 package com.bayu.jwt.service;
 
-public interface CustomUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+public interface CustomUserDetailsService extends UserDetailsService {
 
+    @Override
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
+    public UserDetails loadUserById(Long id);
 }
