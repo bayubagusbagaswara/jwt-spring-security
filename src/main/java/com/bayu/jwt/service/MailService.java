3a3e0c798643e79ab2e5b8a2e7354f4f9714bbd3
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-20 09:48:01 +0700

    add method sendResetLink, sendAccountChangeEmail, and send

--- src/main/java/com/bayu/jwt/service/MailService.java
+++ src/main/java/com/bayu/jwt/service/MailService.java
@@ -1,4 +1,13 @@
 package com.bayu.jwt.service;
 
+import com.bayu.jwt.model.Mail;
+
 public interface MailService {
+
+    public void sendResetLink(String resetPasswordLink, String to);
+
+    public void sendAccountChangeEmail(String action, String actionStatus, String to);
+
+    public void send(Mail mail);
+
 }
