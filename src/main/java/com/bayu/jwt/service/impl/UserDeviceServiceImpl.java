b6b6656e718deaed8bb4945d6320324e1ebdec1f
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-24 09:55:53 +0700

    implement method findDeviceByUserId

--- src/main/java/com/bayu/jwt/service/impl/UserDeviceServiceImpl.java
+++ src/main/java/com/bayu/jwt/service/impl/UserDeviceServiceImpl.java
@@ -3,7 +3,9 @@ package com.bayu.jwt.service.impl;
 import com.bayu.jwt.model.UserDevice;
 import com.bayu.jwt.model.token.RefreshToken;
 import com.bayu.jwt.payload.DeviceInfo;
+import com.bayu.jwt.repository.UserDeviceRepository;
 import com.bayu.jwt.service.UserDeviceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,9 +13,16 @@ import java.util.Optional;
 @Service
 public class UserDeviceServiceImpl implements UserDeviceService {
 
+    private final UserDeviceRepository userDeviceRepository;
+
+    @Autowired
+    public UserDeviceServiceImpl(UserDeviceRepository userDeviceRepository) {
+        this.userDeviceRepository = userDeviceRepository;
+    }
+
     @Override
     public Optional<UserDevice> findDeviceByUserId(Long userId, String deviceId) {
-        return Optional.empty();
+        return userDeviceRepository.findByUserIdAndDeviceId(userId, deviceId);
     }
 
     @Override
