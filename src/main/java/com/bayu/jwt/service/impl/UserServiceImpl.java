af32faae4905f8dcf3a579a9dba5f5e535b63a70
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-24 09:11:52 +0700

    implement method save

--- src/main/java/com/bayu/jwt/service/impl/UserServiceImpl.java
+++ src/main/java/com/bayu/jwt/service/impl/UserServiceImpl.java
@@ -47,7 +47,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User save(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
