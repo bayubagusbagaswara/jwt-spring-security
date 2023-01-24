16791cb007f81bc91908e8ef07ca35f48bccdcd0
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-24 15:00:40 +0700

    add method contract sendEmailVerification

--- src/main/java/com/bayu/jwt/service/MailService.java
+++ src/main/java/com/bayu/jwt/service/MailService.java
@@ -4,6 +4,8 @@ import com.bayu.jwt.model.Mail;
 
 public interface MailService {
 
+    public void sendEmailVerification(String emailVerificationUrl, String to);
+
     public void sendResetLink(String resetPasswordLink, String to);
 
     public void sendAccountChangeEmail(String action, String actionStatus, String to);
