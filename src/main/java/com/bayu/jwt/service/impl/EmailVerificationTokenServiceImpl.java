f699efbf2a2c7836a0f87e8888e9eae040a8dcb2
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-24 14:32:43 +0700

    implement method createVerificationToken

--- src/main/java/com/bayu/jwt/service/impl/EmailVerificationTokenServiceImpl.java
+++ src/main/java/com/bayu/jwt/service/impl/EmailVerificationTokenServiceImpl.java
@@ -1,15 +1,46 @@
 package com.bayu.jwt.service.impl;
 
+import com.bayu.jwt.model.TokenStatus;
+import com.bayu.jwt.model.User;
 import com.bayu.jwt.model.token.EmailVerificationToken;
+import com.bayu.jwt.repository.EmailVerificationTokenRepository;
 import com.bayu.jwt.service.EmailVerificationTokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Service
 public class EmailVerificationTokenServiceImpl implements EmailVerificationTokenService {
 
+    private final static Logger logger = LoggerFactory.getLogger(EmailVerificationTokenServiceImpl.class);
 
+    private final EmailVerificationTokenRepository emailVerificationTokenRepository;
+
+    @Value("{app.token.email.verification.duration}")
+    private Long emailVerificationTokenExpiryDuration;
+
+    @Autowired
+    public EmailVerificationTokenServiceImpl(EmailVerificationTokenRepository emailVerificationTokenRepository) {
+        this.emailVerificationTokenRepository = emailVerificationTokenRepository;
+    }
+
+    @Override
+    public void createVerificationToken(User user, String token) {
+        EmailVerificationToken emailVerificationToken = new EmailVerificationToken();
+        emailVerificationToken.setToken(token);
+        emailVerificationToken.setTokenStatus(TokenStatus.STATUS_PENDING);
+        emailVerificationToken.setUser(user);
+        emailVerificationToken.setExpiryDate(Instant.now().plusMillis(emailVerificationTokenExpiryDuration));
+
+        logger.info("Generated Email verification token [" + emailVerificationToken + "]");
+
+        emailVerificationTokenRepository.save(emailVerificationToken);
+    }
 
     @Override
     public EmailVerificationToken updateExistingTokenWithNameAndExpiry(EmailVerificationToken existingToken) {
