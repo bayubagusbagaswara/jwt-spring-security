efe75e3266b227574d3d48aba9e9706fd7683a64
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-24 15:07:17 +0700

    update method sendEmailVerification

--- src/main/java/com/bayu/jwt/service/MailService.java
+++ src/main/java/com/bayu/jwt/service/MailService.java
@@ -1,10 +1,13 @@
 package com.bayu.jwt.service;
 
 import com.bayu.jwt.model.Mail;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
 
 public interface MailService {
 
-    public void sendEmailVerification(String emailVerificationUrl, String to);
+    public void sendEmailVerification(String emailVerificationUrl, String to) throws IOException, TemplateException;
 
     public void sendResetLink(String resetPasswordLink, String to);
 
