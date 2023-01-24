f4adddb2adbd4fe7351c248c69be48914caa1fcc
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-24 10:22:51 +0700

    implement method createRefreshToken and verifyExpiration

--- src/main/java/com/bayu/jwt/service/impl/RefreshTokenServiceImpl.java
+++ src/main/java/com/bayu/jwt/service/impl/RefreshTokenServiceImpl.java
@@ -1,12 +1,15 @@
 package com.bayu.jwt.service.impl;
 
+import com.bayu.jwt.exception.TokenRefreshException;
 import com.bayu.jwt.model.token.RefreshToken;
 import com.bayu.jwt.repository.RefreshTokenRepository;
 import com.bayu.jwt.service.RefreshTokenService;
+import com.bayu.jwt.util.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Service
@@ -30,12 +33,18 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
 
     @Override
     public RefreshToken createRefreshToken() {
-        return null;
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setExpiryDate(Instant.now().plusMillis(refreshTokenDurationMs));
+        refreshToken.setToken(Util.generateRandomUuid());
+        refreshToken.setRefreshCount(0L);
+        return refreshToken;
     }
 
     @Override
     public void verifyExpiration(RefreshToken token) {
-
+        if (token.getExpiryDate().compareTo(Instant.now()) < 0) {
+            throw new TokenRefreshException(token.getToken(), "Expired token. Please issue a new request");
+        }
     }
 
     @Override
