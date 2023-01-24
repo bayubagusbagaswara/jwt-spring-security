01a099693bdc82520a7f90b736297ccc71cec9be
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-24 10:45:41 +0700

    implement method getValidToken

--- src/main/java/com/bayu/jwt/service/impl/PasswordResetTokenServiceImpl.java
+++ src/main/java/com/bayu/jwt/service/impl/PasswordResetTokenServiceImpl.java
@@ -1,9 +1,13 @@
 package com.bayu.jwt.service.impl;
 
+import com.bayu.jwt.exception.ResourceNotFoundException;
 import com.bayu.jwt.model.PasswordResetToken;
 import com.bayu.jwt.model.User;
 import com.bayu.jwt.payload.PasswordResetRequest;
+import com.bayu.jwt.repository.PasswordResetTokenRepository;
 import com.bayu.jwt.service.PasswordResetTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,9 +15,25 @@ import java.util.Optional;
 @Service
 public class PasswordResetTokenServiceImpl implements PasswordResetTokenService {
 
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
+
+    @Value("${app.token.password.reset.duration}")
+    private Long expiration;
+
+    @Autowired
+    public PasswordResetTokenServiceImpl(PasswordResetTokenRepository passwordResetTokenRepository) {
+        this.passwordResetTokenRepository = passwordResetTokenRepository;
+    }
+
     @Override
     public PasswordResetToken getValidToken(PasswordResetRequest request) {
-        return null;
+        String tokenID = request.getToken();
+        PasswordResetToken token = passwordResetTokenRepository.findByToken(tokenID)
+                .orElseThrow(() -> new ResourceNotFoundException("Password Reset Token", "Token Id", tokenID));
+
+        matchEmail(token, request.getEmail());
+        verifyExpiration(token);
+        return token;
     }
 
     @Override
