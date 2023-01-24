3cf00942383147e60ca5a619eb25f5a2ca2f60a0
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-24 09:53:53 +0700

    implement method findAll

--- src/main/java/com/bayu/jwt/service/impl/RoleServiceImpl.java
+++ src/main/java/com/bayu/jwt/service/impl/RoleServiceImpl.java
@@ -1,7 +1,9 @@
 package com.bayu.jwt.service.impl;
 
 import com.bayu.jwt.model.Role;
+import com.bayu.jwt.repository.RoleRepository;
 import com.bayu.jwt.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -9,8 +11,16 @@ import java.util.Collection;
 @Service
 public class RoleServiceImpl implements RoleService {
 
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
     @Override
     public Collection<Role> findAll() {
-        return null;
+        return roleRepository.findAll();
     }
+
 }
