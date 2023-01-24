e2a2aadaffc32d666ccdcda91f6219ead80c5847
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-24 10:11:54 +0700

    implement method findByToken

--- src/main/java/com/bayu/jwt/service/impl/RefreshTokenServiceImpl.java
+++ src/main/java/com/bayu/jwt/service/impl/RefreshTokenServiceImpl.java
@@ -1,17 +1,26 @@
 package com.bayu.jwt.service.impl;
 
 import com.bayu.jwt.model.token.RefreshToken;
+import com.bayu.jwt.repository.RefreshTokenRepository;
 import com.bayu.jwt.service.RefreshTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class RefreshTokenServiceImpl implements RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Value("${app.token.refresh.duration}")
+    private Long refreshTokenDurationMs;
 
     @Override
     public Optional<RefreshToken> findByToken(String token) {
-        return Optional.empty();
+        return refreshTokenRepository.findByToken(token);
     }
 
     @Override
